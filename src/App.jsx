9447182794fac6f9d85303ec47c08f5113d1d344
Smit Sojitra <smitsojitra810@gmail.com>
2024-04-06 14:25:10 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Cart from "./Pages/Cart";
 
 function App() {
   return (
-    <div>
+    <>
       <div className="bg-slate-800">
         <Navbar/>
       </div>
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/cart" element={<Cart/>}/>
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Store } from './Redux/Store';
 import { Provider } from "react-redux";
